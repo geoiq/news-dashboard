83d8ec21377c26a55b5de48396b3f56068917968
Matt Constantine <designmc@gmail.com>
2009-02-16 13:33:45 -0800

    deploy no longer asks for domain

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,8 @@ set :group, "wheel"
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ec2-geocommons", "id_rsa")] 
 
 # set(:hostname) { Capistrano::CLI.ui.ask("\n>> Hostname (e.g. ec2-75-101-250-183.compute-1.amazonaws.com): ") }
-set(:hostname) { Capistrano::CLI.ui.ask("\n>> Domain Name (e.g. customer.geoappliance.com): ") }
+#set(:hostname) { Capistrano::CLI.ui.ask("\n>> Domain Name (e.g. customer.geoappliance.com): ") }
+set(:hostname) "news.geocommons.com"
 
 role :app, hostname
 role :web, hostname
